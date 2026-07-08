bc2555344cfdd416ee6b3c9f77922042948cfe7b
Dusk <2581683887@qq.com>
2026-07-09 07:23:50 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box">
+        <h1>我是App</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.box{
+    width: 400px;
+    height: 400px;
+    background-color: #56d0e9;
+    h1{
+        color: white;
+    }
+}
+</style>
